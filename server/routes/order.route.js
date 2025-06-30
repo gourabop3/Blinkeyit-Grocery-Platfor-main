72383f063726dbe655c74adf14d67b27a1ad2900
gourabop3 <gourabmullick200@gmail.com>
2025-06-30 19:31:22 +0530

    Update order.route.js

--- server/routes/order.route.js
+++ server/routes/order.route.js
@@ -2,12 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const auth = require("../middlewares/auth.middleware.js");
-const admin = require("../middlewares/admin.middleware.js");
-
 const {
   CashOnDeliveryOrderController,
   getOrderDetailsController,
-  getAllOrdersController, // ✅ Import new controller
   paymentController,
   webhookStripe,
 } = require("../controllers/order.controller.js");
@@ -15,11 +12,6 @@ const {
 router.post("/cash-on-delivery", auth, CashOnDeliveryOrderController);
 router.post("/checkout", auth, paymentController);
 router.post("/webhook", webhookStripe);
-
-// ✅ For normal user to see their own orders
 router.get("/order-list", auth, getOrderDetailsController);
-
-// ✅ For admin to see ALL orders
-router.get("/admin/order-list", auth, admin, getAllOrdersController);
 
 module.exports = router;
